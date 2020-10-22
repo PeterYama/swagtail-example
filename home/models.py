23812c23ed0601ef17bb431be1fbf7e82024d98a
PeterYama <yamamoto265@gmail.com>
2020-10-22 20:08:30 +1100

    Test

--- home/models.py
+++ home/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
-
+from . import blocks
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    author = models.CharField(max_length=255, null=True, blank=True)
+    date = models.DateField("Post date", null=True, blank=True)
+    config = models.CharField(max_length=255, default="default_config")
+
+    content = StreamField([
+        ("title_and_text", blocks.PageBlock(null=True, blank=True)),
+        ('user', blocks.UserBlock(null=True, blank=True, icon='user')),
+    ])
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        StreamFieldPanel('content', classname="full"),
     ]
